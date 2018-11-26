e21d87e52b15dc93b7455c1be5def10def6d2603
Sara Walker <sarawall1187@gmail.com>
2018-11-26 21:36:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,15 @@
 def turn_count(board)
   board.select {|turn| turn == "X" || turn ==# board.count{|token| token == "X" || token == "O"} "O"}.length
+  
   # board.count{|token| token == "X" || token == "O"}
+  
   # turns = 0
   # board.each do |turn|
   #   #if turn == "X" || turn == "O"
   #   turns += 1 
 #   end
 # end
+
 # turns
 end
 
